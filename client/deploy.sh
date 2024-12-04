ef808d4b05f1ad4f2b2626ef94f5e1a6200d1f7c
binh.nguyenngoc <binh.nguyenngoc@fssc.com.vn>
2024-12-04 09:38:26 +0700

    build

--- client/deploy.sh
+++ client/deploy.sh
@@ -2,6 +2,6 @@ echo "Building app..."
 npm run build   
 
 echo "Deploy files to server..."
-scp -r ./build/* root@cokhithvietnam:/var/www/binhhuun/html/
+scp -r ./build/* root@203.171.25.107:/var/www/booktrieutran/html/
 
 echo "Done!"
