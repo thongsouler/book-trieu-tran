f43e306c15d807a00aa269c1a1b9ffe739b1fe8f
Hoàng Thông <46652538+thongsouler@users.noreply.github.com>
2024-12-10 15:18:13 +0700

    updated

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -1,20 +1,28 @@
-import { Typography, useTheme } from '@mui/material';
+import { useTheme } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import LogoW from "../../assets/images/logo.png";
 
 const Logo = ({ scrollTo }) => {
   const theme = useTheme();
   const navigate = useNavigate(); // Hook để điều hướng
 
   return (
-    <Typography fontWeight="700" fontSize="1.7rem" onClick={() => {
-      if (window.location.pathname !== '/') {
-        navigate('/'); // Chuyển hướng tới trang chủ nếu không phải trang chủ
-      } else if (scrollTo) {
-        scrollTo('idScrollDes'); // Nếu đã ở trang chủ, thực hiện cuộn
-      }
-    }}>
-      Cánh <span style={{ color: theme.palette.primary.main }}>Diều</span>
-    </Typography>
+    <img
+      src= {LogoW}
+      alt="Logo"
+      style={{
+        cursor: 'pointer', 
+        height: '55px', 
+        width: 'auto',
+      }}
+      onClick={() => {
+        if (window.location.pathname !== '/') {
+          navigate('/'); // Chuyển hướng tới trang chủ nếu không phải trang chủ
+        } else if (scrollTo) {
+          scrollTo('idScrollDes'); // Nếu đã ở trang chủ, thực hiện cuộn
+        }
+      }}
+    />
   );
 };
 
